4b2dd0a146f77c1c533b58328f02901b64278df8
Handa-dev <emailsukhbirsingh@gmail.com>
2021-06-25 22:02:20 +1000

    pattern added

--- 20210616_basiclayout/includes/action/print.php
+++ 20210616_basiclayout/includes/action/print.php
@@ -1,20 +1,44 @@
 <?php
-if(isset($_POST['patternId']))
+
+//$i=$_POST['patternId'];
+if(isset($_POST['patternId']) && !empty($_POST['patternId']))
 {
-    $i=$_POST['patternId'];
-    $j=1;
-    //exit;
-    for($k=1;$k<=$i;$k++) 
+    if(isset($_POST['symbolId']) && !empty($_POST['symbolId']))
     {
-        //echo $j;
-        //exit;
-        
-        for($j=1;$j<=$k;$j++)
+        $i=$_POST['patternId'];
+        $s=$_POST['symbolId']; // for printing the symbol
+        $j=1;
+    //exit;
+        for($k=1;$k<=$i;$k++) 
         {
-            echo "$j";
+            //echo $j;
+            //exit;
+        
+            for($j=1;$j<=$k;$j++)
+            {
+                echo "$s";
+            }
+            echo "<br>";
         }
-      
-    echo "<br>";
+    }
+    else
+    {
+        $i=$_POST['patternId'];
+        $j=1;
+        for($k=1;$k<=$i;$k++) 
+        {     
+            for($j=1;$j<=$k;$j++)
+            {
+                echo "$j";
+            }
+            echo "<br>";
+        }
     }
 }
-?>
+ else
+    {
+    echo "Go Back and Fill form";
+    }
+    ?>
+    
+    
