1f64795a05dc49fe601a858ae53660c103d614c5
DevShivam007 <handa.edtech02@gmail.com>
2021-06-25 18:28:57 +1000

    logicIncreasingPatternPrint

---
+++ 20210616_basiclayout/includes/action/incresingPattern.php
@@ -0,0 +1,8 @@
+<?php
+?>
+<form action='print.php' method="POST">
+<h1> Incresing Pattern </h1>
+Enter number <input type="text" name="patternId" > <br><br>
+<input type="submit" name="save"> 
+</form>
+<a href="../index.php"> BACK</a>
