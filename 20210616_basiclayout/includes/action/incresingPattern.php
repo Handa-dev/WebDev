95a2fcc488f81bdf2b7d288fe7ff758bb9211e27
DevShivam007 <handa.edtech02@gmail.com>
2021-06-26 16:54:13 +1000

    checkedIncreasingPattern

--- 20210616_basiclayout/includes/action/incresingPattern.php
+++ 20210616_basiclayout/includes/action/incresingPattern.php
@@ -1,8 +1,8 @@
 <?php
 ?>
 <form action='print.php' method="POST">
-<h1> Incresing Pattern </h1>
-Enter number <input type="text" name="patternId" > <br><br>
+<h1> Incraesing Pattern </h1>
+Enter number: <input type="text" name="patternId"> <br><br>
+Symbol to Print: <input type="text" name="symbolId"> <br><br>
 <input type="submit" name="save"> 
 </form>
-<a href="../index.php"> BACK</a>
