c79ea0a4338373120bb35264501780597e30b266
Handa-dev <emailsukhbirsingh@gmail.com>
2021-07-01 15:22:43 +1000

    intro PHP File System Overview

--- 20210616_basiclayout/index.php
+++ 20210616_basiclayout/index.php
@@ -1,41 +1,50 @@
-<h1>PHP Include and Require</h1>
-PHP allows us to create various elements and functions, which are used several times in many pages. It takes much time to script these functions in multiple pages. Therefore, use the concept of file inclusion that helps to include files in various programs and saves the effort of writing code multiple times.
-<h2>Both include and require are identical to each other, except failure.</h2>
-include only generates a warning, i.e., E_WARNING, and <b>continue</b> the execution of the script.
-require generates a fatal error, i.e., E_COMPILE_ERROR, <b>and stop</b> the execution of the script.
-<h2>Example</h2>
-<a href="https://checkpte.com/faq.html">FAQ</a> |     
-<a href="https://checkpte.com/">HOME</a> |     
-<?php 
-//include("index.php");
-//exit;
- ?>  
-<h1>PHP Cookie</h1>
-PHP cookie is a small piece of information which is stored at client browser. <br>It is used to recognize the user.<br>
-Cookie is created at server side and saved to client browser.<br>
- Each time when client sends request to the server, cookie is embedded with request.<br>
-  Such way, cookie can be received at the server side.<br>
-<b>cookies in php can be created, sent and received at server end.<b>
-<h2>PHP setcookie() function</h2>
-PHP setcookie() function is used to set cookie with HTTP response.<b>
-Once cookie is set, you can access it by $_COOKIE superglobal variable.<b>
+<h1>PHP file Handling</h1>
+PHP File System allows us to <br>
+- create file, <br>
+- read file line by line, <br>
+- read file character by character, <br>
+- write file, <br>
+- append file, <br>
+- delete file <br>
+- close file.<br>
+<h2>PHP Open File - fopen()</h2>
+The PHP fopen() function is used to open a file. <br>
+<?php
+$handle = fopen("C:\\xampp\\htdocs\\WebDev-1\\20210616_basiclayout\\myfile.txt", "r"); 
+?>
+<h2>PHP Close File - fclose()</h2>
+The PHP fclose() function is used to close an open file pointer. <br>
 <?php  
-setcookie("user", "Sonoo");  
-?>  
- 
+fclose($handle);  
+?> 
+<h2>PHP Read File - fread()</h2>
+The PHP fread() function is used to read the content of the file. <br>
+It accepts two arguments: resource and file size. <br>
+Follow example shows how to get the content of an existing file <br>
+<?php    
+$filename = "C:\\xampp\\htdocs\\WebDev-1\\20210616_basiclayout\\myfile.txt";    
+$handle = fopen($filename, "r");//open file in read mode    
+$contents = fread($handle, filesize($filename));//read file    
+
+echo $contents;//printing data of file  
+fclose($handle);//close file    
+?>    
+<h2>PHP Write File - fwrite()</h2>
+The PHP fwrite() function is used to write content of the string into file. <br>
 <?php  
-if(!isset($_COOKIE["user"])) {  
-    echo "Sorry, cookie is not found!";  
-} else {  
-    echo "<br/>Cookie Value: " . $_COOKIE["user"];  
-}  
+$fp = fopen('C:\\xampp\\htdocs\\WebDev-1\\20210616_basiclayout\\myfile.txt', 'w');//open file in write mode  
+fwrite($fp, 'hello ');  
+fwrite($fp, 'php file');  
+fclose($fp);  
+  
+echo "File written successfully";  
 ?>  
-<h1>PHP Session</h1>
-PHP session is used to store and pass information from one page to another temporarily (until user close the website).<br>
+<h2>PHP Delete File - unlink()</h2>
+The PHP unlink() function is used to delete file. <br>
+<?php    
+unlink('C:\\xampp\\htdocs\\WebDev-1\\20210616_basiclayout\\myfile.txt');  
+   
+echo "File deleted successfully";  
+?> 
 
-PHP session technique is widely used in shopping websites where we need to store and pass cart information <br>
-e.g. username, product code, product name, product price etc from one page to another.<br>
 
-<b>PHP session creates unique user id for each browser to recognize the user and avoid conflict between multiple browsers.</b><br>
-<li><a href="../20210616_basiclayout/includes/session1.php">session1.php</li>
-<li><a href="../20210616_basiclayout/includes/sessionCounter.php">sessionCounter</li>
