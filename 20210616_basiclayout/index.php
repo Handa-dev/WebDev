44c4a3799bca955955d61166272b6d4360cdd0e5
Handa-dev <emailsukhbirsingh@gmail.com>
2021-07-02 16:58:31 +1000

    intro_PHP_OOPs

--- 20210616_basiclayout/index.php
+++ 20210616_basiclayout/index.php
@@ -1,38 +1,50 @@
-<h2>PHP Download File</h2>
-PHP enables you to download file easily using built-in readfile() function. <br>
-The readfile() function reads a file and writes it to the output buffer. <br>
-<h3>PHP readfile() function</h3>
-The readfile() function reads a file and writes it to the output buffer. <br>
-<h4>Syntax</h4>
-int readfile ( string $filename [, bool $use_include_path = false [, resource $context ]] )  
-
-
-$filename: represents the file name
-
-$use_include_path: it is the optional parameter.
-It is by default false. 
-You can set it to true to the search the file in the included_path.
-
-$context: represents the context stream resource.
-
-int: it returns the number of bytes read from the file.
-<!--Need further investigation -->
-
-<h5> File: download1.php</h5>
+<h1>PHP WITH OOPS CONCEPT</h1>
+Object-oriented programming is a programming model organized <br>
+around Object rather than the actions and data  rather than logic. <br>
+<h2>Class:</h2>
+A class is an entity that determines how an object will behave and what the object will contain. <br>
+In other words, it is a blueprint or a set of instruction to build a specific type of object. <br>
+In PHP, declare a class using the class keyword, followed by the name of the class and a <br>
+set of curly braces ({}). <br>
+<h2>Syntax to Create Class in PHP</h2>
 <?php  
-$file_url = 'https://www.checkpte.com/';  
-header('Content-Type: application/octet-stream');  
-header("Content-Transfer-Encoding: utf-8");   
-header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\"");   
-readfile($file_url);  
+/*
+class MyClass  
+    {  
+        // Class properties and methods go here  
+    }  
+*/
 ?>  
-<h5>PHP Download File Example: Binary File File: ; download2.php</h5>
+<h2>Important note:</h2>
+In PHP, to see the contents of the class, use var_dump(). <br>
+The var_dump() function is used to display the structured information (type and value) <br>
+about one or more variables. <br>
 
+var_dump($obj);  
+<br>
+<h2>Object:</h2>
+A class defines an individual instance of the data structure. <br>
+We define a class once and then make many objects that belong to it. <br>
+Objects are also known as an instance. <br>
+An object is something that can perform a set of related activities. <br>
 <?php  
-
-$file_url = 'http://www.myremoteserver.com/file.exe';  
-header('Content-Type: application/octet-stream');  
-header("Content-Transfer-Encoding: Binary");   
-header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\"");   
-readfile($file_url);  
+class MyClass  
+{  
+        // Class properties and methods go here  
+}  
+$obj = new MyClass;  
+var_dump($obj);  
+?> 
+<h2>Example of class and object:</h2>
+<?php  
+class demo  
+{  
+        private $a= "hello Shivam";  
+        public function display()  
+        {  
+        echo $this->a;  
+        }  
+}  
+$obj = new demo();  
+$obj->display();  
 ?>  
